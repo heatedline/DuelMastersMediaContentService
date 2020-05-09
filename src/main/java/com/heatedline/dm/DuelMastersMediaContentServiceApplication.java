c7676fe1c1e54ab98df154690909f17e6af5ee2c
supratim <supratim.c@finlabsindia.com>
2020-05-09 19:30:36 +0530

    first commit

---
+++ src/main/java/com/heatedline/dm/DuelMastersMediaContentServiceApplication.java
@@ -0,0 +1,13 @@
+package com.heatedline.dm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DuelMastersMediaContentServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DuelMastersMediaContentServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/heatedline/dm/DuelMastersMediaContentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heatedline.dm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DuelMastersMediaContentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
